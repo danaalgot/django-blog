8444d66f297330020d42d8b628a2b3682de38e80
danaalgot <dana@algot.ca>
2020-02-06 21:25:53 -0700

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def post_list(request): #takes the request and retuns it value it gets from another function render that will render our template
+    return render(request, 'blog/post_list.html', {})
+
 # Create your views here.
